229386f7646e7591b7d1f4d4493ab5d9e4e214f0
riteshideas <ritesh.sathiyamoorthi@gmail.com>
2022-05-08 10:53:04 +0800

    Added comments to TrappingRainwater.c

--- TrappingRainwater.c
+++ TrappingRainwater.c
@@ -1,10 +1,13 @@
+// Includind libraries
 #include <stdio.h> 
 #include <stdbool.h>
 #include <stdlib.h>
 
 
 int trap(int* height, int heightSize);
+
 bool validateForZeroValues(int* height, int heightSize);
+
 
 int main() {
 	int height[132] = {1000,999,998,997,996,995,994,993,992,991,990,989,988,987,986,985,984,983,982,981,980,979,978,977,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966,966};
@@ -12,6 +15,8 @@ int main() {
 	return 0;
 }
 
+
+// To make sure that the height Values are not in accending order
 bool validateForZeroValues(int* height, int heightSize) {
 	int desendingRepeat = 0;
 	bool returnValue = false;
@@ -29,8 +34,11 @@ bool validateForZeroValues(int* height, int heightSize) {
 	return returnValue;
 }
 
+
+// Main trap functions
 int trap(int* height, int heightSize) {
 
+	// Initilizing all the variables
 	int rainwater = 0;
 	int highestElement = 0;
 
@@ -49,22 +57,25 @@ int trap(int* height, int heightSize) {
 
     bool firstHalfBool = false;
     bool secondHalfBool = false;
-    
+
+    // Finding the highest element
     for (i = 0; i < heightSize;i++) {
         if (height[i] > highestElement) highestElement = height[i];
     }
 
-    if (validateForZeroValues(height, heightSize)) {
-    	return 0;
-    }
+
+    // Checking that the heightValues are not in accending order
+    if (validateForZeroValues(height, heightSize)) return 0;
 
 
-    if (highestElement > 1) {
+
+    if (highestElement > 1) { // If statment to make sure that the gretest heightValue is greater than 1
     	
+    	// Initilizing the 2 dimentional infoList
         int infoList[highestElement][heightSize];
         int infoList2[highestElement][heightSize];
     
-        
+        // Adding values to infoList2 about from the height[]
         for (a = 0; a < highestElement; a++) {
             for (b = 0; b < heightSize; b++) {
     
@@ -76,7 +87,8 @@ int trap(int* height, int heightSize) {
                 }
             }
         }
-    
+    	
+    	// Reversing the values so that it is easier and saving it to the infoList
         for (c = 0; c < highestElement; c++) {
             for (d = 0; d < heightSize; d++) {
                 infoList[c][d] = infoList2[highestElement-(c+1)][d];
@@ -84,16 +96,26 @@ int trap(int* height, int heightSize) {
         }
     
     
-    
+    	
+    	// Iterating throught the infoList
         for (e = 0; e < highestElement; e++) {
             for (f = 0; f < heightSize; f++) {
-    
+
+    			/*
+				1 : Solid
+				0: Air
+    			*/
+
                 firstHalfBool = false;
                 secondHalfBool = false;
-    
+    			
+
+    			// Making sure that the index is 0, and making sure that the index is not at the start nor at the end
                 if (infoList[e][f] == 0 && f != 0 && f != heightSize - 1) {
     
-    
+    				
+
+                	// Splitting the infoList in to 2 array and saving them to firstHalf and secondHalf
                     splitPoint = f;
                     int firstHalf[f + 1];
                     int secondHalf[heightSize - f - 1];
@@ -107,34 +129,33 @@ int trap(int* height, int heightSize) {
                             z++;
                         }
                     }
-
-                    if(validateForZeroValues(firstHalf,f)) return 0;
     
     
+    				// Checking if 1 is in firstHalf[]
                     for (g = 0; g < f; g++) {
                         if (firstHalf[g] == 1) {
                             firstHalfBool = true;
                         }
                     }
     
+
+    				// Checking if 1 is in secondHalf[]
                     for (h = 0; h < (heightSize - f - 1); h++) {
                         if (secondHalf[h] == 1) {
                             secondHalfBool = true;
                         }
                     }
     
-                    //printf("First Bool: %d, Second Bool: %d\n", firstHalfBool, secondHalfBool);
+
+                    // If there is 1 in firstHalf and secondHalf, add 1 to rainwater
                     if (firstHalfBool && secondHalfBool) rainwater ++;
     
                 }
-                //printf("%d: %d %d   ", infoList[e][f], firstHalfBool, secondHalfBool);
     
             }
-            //printf("\n");
         }
 	
     }
-	//printf("Rainwater : %d\n", rainwater);
     
 	return rainwater;
 }
